9038edd39bc7c5327621ed18b99133b35cd06518
Sabadis <dan.sabadis@siemens.com>
2018-03-21 20:16:33 +0200

    done viewchild

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+import { NgModel } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, AfterViewInit {
     pageTitle: string = 'Product List';
     
     showImage: boolean;
@@ -19,18 +21,52 @@ export class ProductListComponent implements OnInit {
     filteredProducts: IProduct[];
     products: IProduct[];
 
-    // listFilter: string;  // superseeded by getter-setter see immediately below
-    private _listFilter: string;
-    get listFilter(): string {
-        return this._listFilter;
+    @ViewChild('filterElement') filterElementRef: ElementRef;   // these get access to the corresponding DOM
+    private _sub: Subscription;
+    //@ViewChildren('filterElement, nameElement') filterElementRefs: QueryList<ElementRef>;
+    @ViewChildren(NgModel) filterElementRefs: QueryList<ElementRef>;
+
+    listFilter: string;  // superseeded by getter-setter see immediately below
+    // private _listFilter: string;
+    // get listFilter(): string {
+    //     return this._listFilter;
+    // }
+
+    // set listFilter(value: string)  {
+    //     this._listFilter = value;
+    //     this.performFilter(this._listFilter);
+    // }
+
+    //@ViewChild(NgModel) filterInput: NgModel;   // this gets access to the data
+    private _filterInput: NgModel;
+    get filterInput(): NgModel {
+        return this._filterInput;
     }
 
-    set listFilter(value: string)  {
-        this._listFilter = value;
-        this.performFilter(this._listFilter);
+    @ViewChild(NgModel)
+    set filterInput(value: NgModel)  {
+        this._filterInput = value;
+        if(this.filterElementRef)
+            this.filterElementRef.nativeElement.focus();
+
+        if(this.filterInput && !this._sub)
+        this._sub = this.filterInput.valueChanges.subscribe(() => {
+                this.performFilter(this.listFilter);       
+            });
     }
 
-    constructor(private productService: ProductService) { }
+    constructor(private productService: ProductService) { 
+        // in the constructor the view is not yet rendered
+    }
+
+    ngAfterViewInit(): void {
+        console.log(this.filterElementRefs); 
+        console.log(this.filterInput); 
+        // this.filterElementRef.nativeElement.focus();
+        // this.filterInput.valueChanges.subscribe(() => {
+        //     this.performFilter(this.listFilter);       
+        // });
+    }
 
     ngOnInit(): void {
         this.productService.getProducts().subscribe(
